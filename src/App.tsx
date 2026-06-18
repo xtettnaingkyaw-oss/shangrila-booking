35e84f0a36907601565c27d7d6d73782b56f993a
xtettnaingkyaw-oss <xtettnaingkyaw@gmail.com>
2026-06-18 10:22:43 +0630

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -178,7 +178,7 @@ function CustomerBooking() {
 
         {/* Payment Section (Dynamic) */}
         <div className="bg-[#064E3B]/50 p-5 rounded border border-yellow-600/50 mt-6">
-          <h3 className="font-bold flex items-center mb-4 text-yellow-500"><CreditCard className="w-5 h-5 mr-2"/> စရန်ငွေ လွှဲရန်</h3>
+          <h3 className="font-bold flex items-center mb-4 text-yellow-500"><CreditCard className="w-5 h-5 mr-2"/> စရံငွေ လွှဲရန်</h3>
           
           <div className="mb-4">
             <label className="block mb-2 text-sm">ငွေလွှဲမည့် စနစ် ရွေးချယ်ရန် (Payment Method)</label>
@@ -212,7 +212,7 @@ function CustomerBooking() {
           )}
 
           <div>
-            <label className="block mb-2 text-sm">ငွေလွှဲ Transaction ID (နောက်ဆုံး ၆ လုံး)</label>
+            <label className="block mb-2 text-sm">စရံငွေပေးချေမှုပြုလုပ်ပြီးပါက ပြေစာတွင်ပါသော ငွေလွှဲ Transaction ID (နောက်ဆုံး ၆ လုံး) ကို​ အောက်မှာရိုက်ထည့်ပေးပါ</label>
             <input required type="text" name="txId" maxLength={6} minLength={6} placeholder="e.g. 123456" 
               value={formData.txId} onChange={handleChange} 
               className="w-full p-3 bg-[#022c22] border border-[#D4AF37]/50 rounded focus:outline-none focus:border-[#D4AF37] text-center text-xl tracking-widest text-white" />
