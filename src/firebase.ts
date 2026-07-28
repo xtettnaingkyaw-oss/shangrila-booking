73d86e6b181a0f0e24b3476cc40b3f76122ab4c5
xtettnaingkyaw-oss <xtettnaingkyaw@gmail.com>
2026-07-28 09:48:59 +0630

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,19 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// Vercel က Key တွေကို တကယ် မြင်/မမြင် စစ်ဆေးပေးမည့် အပိုင်း
-console.log("Firebase Keys Status:", {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY ? "✅ ရောက်သည်" : "❌ မရောက်ပါ",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ? "✅ ရောက်သည်" : "❌ မရောက်ပါ",
-});
-
+// Vercel ကို မသုံးတော့ဘဲ တိုက်ရိုက် ချိတ်ဆက်မည့်စနစ်
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyD72t-U4ZQY1DVmsxj9O2Vu_XXXXKxwlKo", 
+  authDomain: "shangrila-online-booking-app.firebaseapp.com",
+  projectId: "shangrila-online-booking-app",
+  storageBucket: "shangrila-online-booking-app.firebasestorage.app",
+  messagingSenderId: "696764910771",
+  appId: "1:696764910771:web:04fb68544c4db32ff9b4c6"
 };
 
 const app = initializeApp(firebaseConfig);
