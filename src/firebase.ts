6ff32b60de157c7990d56bd768478a0cd1496128
xtettnaingkyaw-oss <xtettnaingkyaw@gmail.com>
2026-07-28 10:26:08 +0630

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyD72t-U4ZQY1DVmsxj902Vu_XXXXXxw1Ko",
+  authDomain: "shangrila-online-booking-app.firebaseapp.com",
+  projectId: "shangrila-online-booking-app",
+  storageBucket: "shangrila-online-booking-app.firebasestorage.app",
+  messagingSenderId: "696764910771",
+  appId: "1:696764910771:web:04fb68544c4db32ff9b4c6"
 };
 
 const app = initializeApp(firebaseConfig);
