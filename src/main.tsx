09766e09e7b40152433e3c41f1011c53bc3d36c0
xtettnaingkyaw-oss <xtettnaingkyaw@gmail.com>
2026-09-12 00:29:46 +0630

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { AppDataProvider } from './context/AppDataContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppDataProvider>
+      <App />
+    </AppDataProvider>
   </React.StrictMode>
 )
